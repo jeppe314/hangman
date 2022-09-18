30a9431406122572fcda78cee9fb4132304470a3
jeppe314 <jesperlindqvist93@gmail.com>
2022-09-18 12:06:46 +0200

    Basic start page & fetch function, some basic CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import "./App.css"
 import { React, useState } from "react"
-import Start from "./Start.js"
 
 function App() {
   const [gameState, setGameState] = useState(0)
+  const [game, setGame] = useState({})
 
-  return <div className="app">{gameState === 0 && <Start />}</div>
+  function startGame() {
+    fetch("https://api.api-ninjas.com/v1/randomword?type=noun")
+      .then((res) => res.json())
+      .then((data) => setGame(
+        {
+          letters: [...data.word.toUpperCase()],
+          guessedLetters: [],
+          wrongLetters: [],
+        }
+      ))
+      console.log(game)
+      setGameState(1)
+  }
+
+  const startPage = (
+    <div className="start">
+      <h1>HANGMAN</h1>
+      <button onClick={() => startGame()}>Start game</button>
+    </div>
+  )
+
+  return <div className="app">{gameState === 0 && startPage}</div>
 }
 
 export default App
